f4162b5f2b40c16eeb48da3e20fef28fc4983b8a
GJBarts <60287016+GJBarts@users.noreply.github.com>
2020-01-25 09:46:41 +0000

    Add files via upload

---
+++ example-bootstrap/muse/sideproject/Javascript/basics/HelloWorld.js
@@ -0,0 +1,10 @@
+myNumber = 4;
+myString = "Variables are great";
+myBoolean = false;
+
+var myArray = ["What is the meaning of life?", 42, true]
+
+function exampleFunction() {
+    console.log('Hello World');
+    alert("I am embedded into the HTML file!")
+}
